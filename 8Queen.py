59561e9143bb9ae45053efb33536b2c2057e090d
TimEnJoosPWS <munneke.joos@gmail.com>
2016-09-08 10:09:56 +0200

    Update 8Queen.py

--- 8Queen.py
+++ 8Queen.py
@@ -3,11 +3,11 @@ from random import randint
 for k in range(5):
 	totalitems = 0
 	failed = 0
-	board = [["" for i in range(7)] for x in range(7)]
+	board = [["" for i in range(8)] for x in range(8)]
 	solutions = []
-	while totalitems - failed < 8:
-		randX = randint(0,9)
-		randY = randint(0,9)
+	while totalitems - failed <= 8:
+		randX = randint(0,8)
+		randY = randint(0,8)
 		board[randY][randX] = "Q" if not board[randY][randX] == "Q" else failed += 1
 		totalitems += 1
 	solutions[k] = board
