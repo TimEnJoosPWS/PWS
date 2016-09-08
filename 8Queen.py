e4d31644185500c68eb7daa6fad7b7fbd8c7d788
TimEnJoosPWS <munneke.joos@gmail.com>
2016-09-08 10:39:01 +0200

    Update 8Queen.py

--- 8Queen.py
+++ 8Queen.py
@@ -1,6 +1,6 @@
 from random import randint
 
-for k in range(5):
+def createSolution():
 	totalitems = 0
 	failed = 0
 	board = [["" for i in range(8)] for x in range(8)]
@@ -10,4 +10,19 @@ for k in range(5):
 		randY = randint(0,8)
 		board[randY][randX] = "Q" if not board[randY][randX] == "Q" else failed += 1
 		totalitems += 1
-	solutions[k] = board
+	return board
+
+solutions = []
+for i in range(10):
+	solutions[i] = createSolution()
+
+fitnesses = []
+def fitness_count():
+	for i in solutions:
+		if sum(x.count('Q') for x in solutions[i]) == 8:
+			fitnesses[i] = 100
+		else:
+			fitnesses[i] = 10
+	return fitnesses
+
+	
