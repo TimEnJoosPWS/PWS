cf01b6fdae9c086b77abea5d1db9f6847f0333e2
TimEnJoosPWS <munneke.joos@gmail.com>
2016-09-08 22:47:50 +0200

    Update 8Queen.py
    Ik heb ff lopen klooien met die fitnessfunctie, en die is nog flink lastig. Misschien kun je ff kijken of het klopt met alle arrays en indices. Daarnaast heb ik nu alleen nog de fitnessfunctie van de horizontale en verticale velden per bord berekend, nog niet van de diagonalen. Laat trouwens ook ff weten als ik alvast wat naamgeving of andere niet-noodzakelijke syntax aan moet passen.

--- 8Queen.py
+++ 8Queen.py
@@ -16,13 +16,29 @@ solutions = []
 for i in range(10):
 	solutions[i] = createSolution()
 
+	# fitness function, gebaseerd op hoeveel aanvallende koninginnenparen er zijn
+
 fitnesses = []
-def fitness_count():
+total_row_fitness = []
+total_colom_fitness = []
+total_diagonal_fitness = []
+
+def fitness_count(solutions):
 	for i in solutions:
-		if sum(x.count('Q') for x in solutions[i]) == 8:
-			fitnesses[i] = 100
-		else:
-			fitnesses[i] = 10
-	return fitnesses
+		for j in solutions[i]:
+			row_queens = len(solutions[i][j]) 							# berekent de queens op een rij
+			row_fitness[j] = ((row_queens - 1) * row_queens) / 2  		# berekent de fitnesswaarde van de rij met index j
+		total_row_fitness[i] = sum(row_fitness) 						# totale rij-fitheid van het i-de bord is de som van 8 aparte fitnesswaarden (waarbij een lager fitheidsgetal een hogere fitheid betekent)
+
+		for j in solutions[i]:		# geen idee of ik het volgende handig heb gedaan, maar het was best lastig om per kolom de koninginnen te vinden.
+			colom_queens = 0
+			for k in solutions[i][j]:
+				if solutions[i][k][j] == "Q":
+					colom_queens += 1
+			colom_fitness[j] = ((colom_queens - 1) * colom_queens) / 2
+		total_colom_fitness[i] = sum(colom_fitness)
+
+# nu de diagonalen nog. Hier moet je volgens mij NumPy voor gebruiken.
+
 
 	
