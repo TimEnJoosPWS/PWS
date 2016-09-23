21d82a32435f6d517d0b2e338e0248dab9107bbf
TimEnJoosPWS <munneke.joos@gmail.com>
2016-09-23 22:43:51 +0200

    Add files via upload

--- knapsack.py
+++ knapsack.py
@@ -1,5 +1,10 @@
 from random import *
 
-solutions = []
+    # initiation
 
-def createSolution
+n = 8
+def createSolution():
+    decimal_solution = randint(0, 2**n - 1)
+    binary_solution = str(bin(decimal_solution)[2:])
+    solution = (n - len(binary_solution)) * '0' + binary_solution
+    return solution
